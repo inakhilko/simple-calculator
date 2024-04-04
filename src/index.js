420734565a96fbb35450cc65e91217b59712681b
Inna Khilko <innakhilko581@gamil.com>
2024-04-04 19:21:09 +0300

    fix: fix prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function addNumbers() {
-	const num1 = parseFloat(document.getElementById("number1").value);
-	const num2 = parseFloat(document.getElementById("number2").value);
-	const result = num1 + num2;
-	document.getElementById("result").innerText = `Result: ${result}`;
+  const num1 = parseFloat(document.getElementById("number1").value);
+  const num2 = parseFloat(document.getElementById("number2").value);
+  const result = num1 + num2;
+  document.getElementById("result").innerText = `Result: ${result}`;
 }
 
 document.getElementById("calculate").addEventListener("click", addNumbers);
